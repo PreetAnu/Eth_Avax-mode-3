39bb2dffd0b3f22d083aa04dc6105c842c12007f
PreetAnu <158500493+PreetAnu@users.noreply.github.com>
2024-06-26 23:13:44 +0530

    Created Module3.sol

---
+++ Module3.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// the contract owner should be able to mint tokens to a provided address
+// and any user should be able to burn and transfer tokens.
+
+contract METAtoken is ERC20, ERC20Burnable, Ownable {
+    uint public totalTokenSupply;
+
+    constructor(address initialOwner) ERC20("MetaToken", "MTK") Ownable(initialOwner){
+        uint initialTokenSupply = 1000 * 10**18; // Initialize and declare the variable
+        _mint(initialOwner, initialTokenSupply);  
+    }
+    function MintTokens(address recipient, uint amount) public onlyOwner {
+        _mint(recipient, amount);
+        totalTokenSupply += amount;
+    }
+/*
+    // we can alsouse these functions but no need here
+    // because we already import some contract for these function's work.
+    function BurnTokens(uint amount) public {
+        _burn(msg.sender, amount);
+        totalTokenSupply -= amount;
+    }
+
+    function sendTokens(address recipient, uint amount) public {
+        _transfer(msg.sender, recipient, amount);
+    }
+    */
+
+}
